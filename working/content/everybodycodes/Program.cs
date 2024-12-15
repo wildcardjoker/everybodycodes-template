58f5612f8c556c684e1601a86f07f48782088cd4
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-15 11:17:08 +0800

    Add template.

---
+++ working/content/everybodycodes/Program.cs
@@ -0,0 +1,24 @@
+﻿SolvePart1();
+SolvePart2();
+SolvePart3();
+
+void SolvePart1()
+{
+    var input = File.ReadAllText("input1.txt");
+    var result = 0;
+    Console.WriteLine($"Part 1 result: {result}");
+}
+
+void SolvePart2()
+{
+    var input = File.ReadAllText("input2.txt");
+    var result = 0;
+    Console.WriteLine($"Part 2 result: {result}");
+}
+
+void SolvePart3()
+{
+    var input = File.ReadAllText("input3.txt");
+    var result = 0;
+    Console.WriteLine($"Part 3 result: {result}");
+}
---
+++ working/content/everybodycodes/README.md
@@ -0,0 +1,13 @@
+# Quest 1: 
+
+## Part I
+
+### Example based on the following notes:
+
+## Part II
+
+### Example based on the following notes:
+
+## Part III
+
+### Example based on the following notes:
